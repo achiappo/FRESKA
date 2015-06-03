86bc279cc9908bca351170e712d79dc91bc34464
achiappo <chiappo.andrea@gmail.com>
2015-06-03 09:57:26 +0200

    minuit
    modifications have been performed to evaluate the best-fit dark matter density profile parameters using Minuit minimizer on the -log(Likelihood) function of the velocity distribution of the dwarfs' member stars. NB: even the module containing the various auxiliary functions has changed!

--- dwarf_jvalue.py
+++ dwarf_jvalue.py
@@ -1,49 +1,74 @@
 import sys
-from astrotools import get_data,dlike
-from numpy.random import random_sample
+import math
+import numpy as np
+from iminuit import Minuit
+from ATminuit import get_data,get_sigmalos,M
+from scipy.integrate import quadrature
 
-# various parameters definitions
-nparams = 7
-nmax 	= 1000
-ncalls 	= 10000				# number of mcmc accepts
+# paramters
+nmax     = 1000
+nparams  = 7
+pi       = math.pi
+rstar    = 0.3e0                        # 0.3 kpc
+kpctom   = 3.085e19                     # kpc in m
+Msun     = 1.9891e30                    # Solar mass unit
+Mhalo    = 1.e9 * Msun                  # Halo mass
+sigma_MW = 200                          # velocity dispersion of Milky Way in km s^-1
+G        = 6.67e-11*Msun                # m^3 Msun^-1 s^-2          
 
-galaxy  = sys.argv[1]								# get the galaxy name from the command line
-like_val,pa,pmin,pmax = get_data(galaxy)[7:]		# get the data for the galaxy 
+galaxy  = sys.argv[1]                   # get the galaxy name from the command line
 
-# Get initial values for the likelihood function at pa
-laccept = dlike(galaxy)[0]
-xx 		= np.empty([nparams])
+#######################################################################################################
+#                                               MAIN CODE
+#######################################################################################################
 
-output = open('out_'+galaxy+'.dat','w')				# output file for the list of parameters 
+# log(Likelihood) function
+def LogLike(data,a,b,c):
+    x,v,dv,rh,rt,nstars,D,pa = data
+    Mvar,rs,beta = pa[:3]
+    u = pa[-1]
+    arg1 = 0.
+    arg2 = 0.
+    #rcut  = pow(G*Mhalo*pow(D,2)/2./pow(sigma_MW,2),1/3.)
+    p0    = 10.e0**Mvar/M(rstar,rs,a,b,c)
+    for i in range(nstars):
+        s     = get_sigmalos(abs(x[i]),p0,rt,beta,rh,rs,a,b,c)
+        arg1 += 0.5e0*pow(v[i]-u,2)/(pow(dv[i],2)+pow(s,2))
+        arg2 += 0.5e0*math.log(2*pi*pow(dv[i],2)+pow(s,2))
+    dlike  = -arg1-arg2
+    return dlike
 
-# Keep track of accepts and rejects
-accept = 0
-reject = 0 
+# class necessary to fit the LogLike function evaluated at its data points
+class LogLH:    
+    def __init__(self,data):
+        self.data = data
+    
+    def compute(self,a,b,c):
+        return LogLike(self.data,a,b,c)      # FITTED PARAMETERS: a,b,c
 
-#	Core of mcmc code
-print 'Fraction of accepted points,  # of accepted pts, value of likleihood'
-while (accept <= ncalls):
-	if(dlike(galaxy)[0] >= laccept):
-		laccept,p0 = dlike(galaxy)
-		accept += 1
-		output.write(('%14.5e %14.5e %14.5e %14.5e %14.5e %14.5e %14.5e %14.5e')%\
-			(pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6],p0))
-		print ('%20f %20f %20f')%(float(accept)/float(ncalls),accept,laccept)
-	else:
-		ratio = dlike(galaxy)[0]/laccept
-		xxx = random_sample()
-		if(xxx <= ratio):
-			laccept,p0 = dlike(galaxy)
-			accept += 1
-			output.write(('%14.5e %14.5e %14.5e %14.5e %14.5e %14.5e %14.5e %14.5e')%\
-				(pa[0],pa[1],pa[2],pa[3],pa[4],pa[5],pa[6],p0))
-			print ('%20f %20f %20f')%(float(accept)/float(ncalls),accept,laccept)
-		else:
-			reject +=1 
-	for j in range(nparams):
-		xx[j] = random_sample()
-	pa = xx*(pmax-pmin)+pmin
+# integrand of the J factor, i.e. DM density profile squared
+def profile(r,rho0,rs,a,b,c):
+    return pow(rho0/pow(r/rs,a)/pow(1+pow(r/rs,b),(c-a)/b),2.)
 
-output.close()
-print accept,' accepts and ', reject,' rejects',' | total: ',accept+reject  
+#a_init,b_init,c_init = get_data(galaxy)[-1][-4:-1] # extract values from param file
+data = get_data(galaxy)
 
+# building a function object to be passed to Minuit and evaluation of -MLE parameters
+lh = LogLH(data)
+l = (0.1,10.)
+m = Minuit(lh.compute,a=1.,b=1.,c=1.,error_a=0.01,error_b=0.01,error_c=0.01,limit_a=l,limit_b=l,limit_c=l)
+bestfit = m.migrad()
+
+a = bestfit[1][0]["value"]
+b = bestfit[1][1]["value"]
+c = bestfit[1][2]["value"]
+
+'''
+rt = data[4]
+Mvar,rs     = data[-1][:2]
+a,b,c  = 1.,1.5,5.
+rho0    = 10.e0**Mvar/M(rstar,rs,a,b,c)
+Jvalue = quadrature(profile,0.,rt,args=(rho0,rs,a,b,c),maxiter=150)[0]
+
+print math.log10(Jvalue)
+'''
