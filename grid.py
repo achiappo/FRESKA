487400cd40ec4c4dafb92b86a737d95aca2965a3
achiappo <chiappo.andrea@gmail.com>
2015-06-16 16:46:02 +0200

    Update grid.py

--- grid.py
+++ grid.py
@@ -27,9 +27,10 @@ pts = np.load("%s.npy"%dwarf)
 # iii) display colour map
 m = plt.imshow(pts,cmap='rainbow',extent=[1e-2,1e2,1e5,1e9])
 plt.loglog()
-plt.xlabel(r'$r_s$')
-plt.ylabel(r'$rho_0$')
+plt.xlabel(r'$r_s$',fontsize=18)
+plt.ylabel(r'$\rho_0$',fontsize=18)
 plt.grid()
-plt.colorbar(m,pad=0,label='-log(Like)')
+cx = plt.colorbar(m,pad=0)
+cx.set_label(r'$-log(Like)$',fontsize=18)
 plt.scatter(BFparams[1],BFparams[0],s=200,marker='*',c='k')		# display Best-Fit value
 plt.show()
