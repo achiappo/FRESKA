da8e95629afd0eef18b16f53b5afc88abb381d2b
achiappo <chiappo.andrea@gmail.com>
2015-06-19 17:44:59 +0200

    general improvements
    the script now outputs parameter space colour map - where the colour dimension indicates the -log(Like)
    contour lines added

--- grid.py
+++ grid.py
@@ -2,6 +2,7 @@
 # author Andrea Chiappo		<andrea.chiappo@fysik.su.se>
 import sys
 import math
+import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,26 +12,26 @@ import matplotlib.pyplot as plt
 
 # i) extract BF parameters
 dwarf  = sys.argv[1]							# get the galaxy name from the command line
-params = 'rho0','rs'							# best-fit parameters to be extracted from the logfile		
-BFparams = []
-logfile = open('out_%s.txt'%dwarf,'r').readlines()
-for param in params:
-	for line in logfile:
-		if param in line:
-			data = line.strip().split()
-			if len(data) > 4:
-				BFparams.append(float(data[5]))
+
+bestfit = yaml.load(open("output/%s.yaml"%dwarf,"rb"))
+rho0 = bestfit[1][0]["value"]
+rs   = bestfit[1][1]["value"]
 
 # ii) load -log(Like) grid values
-pts = np.load("%s.npy"%dwarf)
+pts = np.load("output/%s.npy"%dwarf)
 
 # iii) display colour map
-m = plt.imshow(pts,cmap='rainbow',extent=[1e-2,1e2,1e5,1e9])
-plt.loglog()
-plt.xlabel(r'$r_s$',fontsize=18)
-plt.ylabel(r'$\rho_0$',fontsize=18)
+m = plt.imshow(np.flipud(pts),cmap='rainbow',extent=[0.1,rs+1.,rho0-1.,rho0+1.])
+ct = plt.contour(np.linspace(0.1,rs+1.,20),np.linspace(rho0-1.,rho0+1.,20),pts)
+plt.clabel(ct,inline=1,fmt='%1.0f',colors='k')
+plt.semilogy()
+plt.xlabel(r'$r_s [kpc]$',fontsize=18)
+plt.ylabel(r'$\rho_0 [M_{sun}  kpc^{-3}]$',fontsize=18)
+plt.yticks([int(rho0),int(rho0+1.)],['%1.0e'%10**int(rho0),'%1.0e'%10**int(rho0+1.)])
 plt.grid()
 cx = plt.colorbar(m,pad=0)
 cx.set_label(r'$-log(Like)$',fontsize=18)
-plt.scatter(BFparams[1],BFparams[0],s=200,marker='*',c='k')		# display Best-Fit value
+plt.scatter(rs,rho0,s=200,marker='*',c='k',label=r'$r_s = $ %1.1f, $log_{10}(\rho_0) = $ %1.1f'%(rs,rho0))
+plt.legend(scatterpoints=1,loc='lower right',fontsize=12)
+plt.savefig('output/%s_im.png'%dwarf,dpi=100,format='png')
 plt.show()
