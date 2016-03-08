772cd908db385048a286de79420aa0ef63d3e68e
achiappo <chiappo.andrea@gmail.com>
2016-03-08 10:46:39 +0100

    setup is set to generate the C library for simulationOM.ipynb

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize(["functions_sim.pyx"])
+    ext_modules = cythonize(["functions_simOM.pyx"])
 )
