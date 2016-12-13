corrected method set_free() of fitter.py module. Now it correctly allows to free a parameter by updating the list of free_pars of the Loglikelihood instance and automatically update the settings of the MinuitFitter instance

fitter.py
@@ -23,15 +23,17 @@ class MinuitFitter(object):
             if par not in self.loglike.sigma.params:
                 raise ValueError('%s not a parameter of logLike function.\n'%par\
 				+'\t The parameters are %s'%self.loglike.sigma.params.keys())
-            self.settings['fix_%s'%par] = False
+            else:
+            	self.loglike.set_free(par)
+        self._synch()
 
     def set_fixed(self, parnames):
         parnames=np.array(parnames, ndmin=1, copy=False)
         for par in parnames:
-            if par not in self.loglike.sigma.params:
-                raise ValueError('%s not a parameter of logLike function.\n'%par\
-				+'\t The parameters are %s'%self.loglike.sigma.params.keys())
-            self.settings['fix_%s'%par] = True
+        	if par not in self.loglike.free_pars:
+        		raise ValueError('%s not a free parameter of logLike function.\n'%par\
+        			+'\t The free parameters are %s'%self.loglike.free_pars.keys())
+        	self.settings['fix_%s'%par] = True
 
     def set_value(self, par, value):
         if par not in self.loglike.free_pars:

utils.py
@@ -12,7 +12,7 @@ def load_data(gal):
         rt = float(parameters[3])
         
     x,v,dv = np.loadtxt('../data/velocities/velocities_%s.dat'%gal,dtype=float,usecols=(0,1,2),unpack=True)
-    return x,v,dv,D,rh,rt
+    return x, v, dv, D, rh, rt
 
 def load_gaia(homedir, MockSize, dataSize, dset, mod, D, with_velocity_errors):
     #homedir = '/home/andrea/Desktop/work/DWARF/jvalue/project1/test/Isotrop_Core_nonPlum'
@@ -48,7 +48,7 @@ def load_gaia(homedir, MockSize, dataSize, dset, mod, D, with_velocity_errors):
     
     if MockSize<dataSize:
         idx=np.random.randint(low=dataSize, size=MockSize)
-    R,v,dv=R[idx],v[idx],dv[idx]
+        R, v, dv = R[idx], v[idx], dv[idx]
 
     rh = 1. if mod == 1 or mod == 2 else 0.25
     r0_true = 1.